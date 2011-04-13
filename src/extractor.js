5562be9fdeea18f45a5013355e1830ff9c57fc39
swilliams <scott@krazyyak.com>
2011-04-13 14:22:23 -0700

    Refactored multiple sub elements a bit

--- src/extractor.js
+++ src/extractor.js
@@ -1,5 +1,19 @@
 var E = function() {
 
+    function extractSubElements($elem, obj, name) {
+        var objects = $elem.extractObjects();
+        if (obj[name] === undefined) {
+            obj[name] = objects.length === 1 ? objects[0] : objects;
+        } else {
+            if (obj[name] instanceof Array) {
+                obj[name].push(objects[0]);
+            } else {
+                var tmp = obj[name]
+                obj[name] = [tmp, objects[0]];
+            }
+        }
+    }
+
     return {
         createSetter: function($elem, obj, name) {
             // The value is the data-value property or the text of the element.
@@ -28,19 +42,8 @@ var E = function() {
                     name = $elem.attr('data-property');
                 // If the element is a node, recursively extract it.
                 if ($elem.children('[data-property]').length > 0) {
-                    objects = $elem.extractObjects();
-                    if (obj[name] === undefined) {
-                        obj[name] = objects.length === 1 ? objects[0] : objects;
-                    } else {
-                        if (obj[name] instanceof Array) {
-                            obj[name].push(objects[0]);
-                        } else {
-                            var tmp = obj[name]
-                            obj[name] = [tmp, objects[0]];
-                        }
-                    }
+                    extractSubElements($elem, obj, name);
                 } else {
-                
                     that.createSetter($elem, obj, name);
                 }
             });
