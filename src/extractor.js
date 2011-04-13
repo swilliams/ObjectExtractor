daf03aa4189501151ba6d4e3a1f9c6de9559b6c0
swilliams <scott@krazyyak.com>
2011-04-13 09:47:31 -0700

    Extracted extractor into its own class.

--- src/extractor.js
+++ src/extractor.js
@@ -1,18 +1,23 @@
+var E = {
+    extract: function($target) {
+        var obj = {};
+        $target.children('[data-property]').each(function(i, n) {
+            var $elem = $(n),
+                val = $elem.is('[data-value]') ? $elem.attr('data-value') : $elem.text();
+
+            obj[$(n).attr('data-property')] = val;
+        });
+        return obj;
+    }
+};
+
 (function($){
     $.fn.extractObject = function() {
   
         var objects = [];
         // create the object from every object in the thing
         this.each(function() {
-            var $this = $(this),
-                obj = {};
-            $this.children('[data-property]').each(function(i, n) {
-                var $elem = $(n),
-                    val = $elem.is('[data-value]') ? $elem.attr('data-value') : $elem.text();
-
-                obj[$(n).attr('data-property')] = val;
-            });
-            
+            var obj = E.extract($(this));
             objects.push(obj);
         });
         return objects;
