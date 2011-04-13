fff0beaede47b15f26ae5defc930b42f81d9431d
swilliams <scott@krazyyak.com>
2011-04-13 08:00:17 -0700

    Got basic example working.

--- src/extractor.js
+++ src/extractor.js
@@ -1,3 +1,21 @@
-$(function() {
-    //
-});
+(function($){
+    $.fn.extractObject = function() {
+  
+        var objects = [];
+        // create the object from every object in the thing
+        this.each(function() {
+            var $this = $(this),
+                obj = {};
+            $this.children('[data-property]').each(function(i, n) {
+                var $elem = $(n),
+                    val = $elem.is('[data-value]') ? $elem.attr('data-value') : $elem.text();
+
+                obj[$(n).attr('data-property')] = val;
+            });
+            
+            objects.push(obj);
+        });
+        return objects;
+    };
+})(jQuery);
+
